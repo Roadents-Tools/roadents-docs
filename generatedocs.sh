fcf3ec7b4aa143c09bc1b6f688a71617864119e3
ischeinkman <scheinkman.ilan@gmail.com>
2017-12-25 17:51:07 -0800

    Changed colors to match website.

--- generatedocs.sh
+++ generatedocs.sh
@@ -1,2 +1,2 @@
 sudo widdershins swagger.yaml -o doc-shins-markdown.md -l true -c true --verbose -u widdershins-templates/openapi3/ &&
-sudo shins --inline doc-shins-markdown.md
+sudo shins --inline --customcss doc-shins-markdown.md
