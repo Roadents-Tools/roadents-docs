13f661f7de73dd78a163ce16fd3ac37d1c81f066
ischeinkman <scheinkman.ilan@gmail.com>
2018-01-19 21:17:17 -0800

    Fixed terminology and enum issues.

--- generatedocs.sh
+++ generatedocs.sh
@@ -1,2 +1,2 @@
-sudo widdershins swagger.yaml -o doc-shins-markdown.md -l true -c true --verbose -u widdershins-templates/openapi3/ &&
+sudo widdershins swagger.yaml -o doc-shins-markdown.md --verbose -u widdershins-templates/openapi3/ &&
 sudo shins --inline --customcss doc-shins-markdown.md
