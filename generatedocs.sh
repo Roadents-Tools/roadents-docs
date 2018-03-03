105b7ede506ff74ca1760357b093b36941eb4a40
ischeinkman <scheinkman.ilan@gmail.com>
2018-03-02 16:29:35 -0800

    Generation script now correctly sets up environment before running.

--- generatedocs.sh
+++ generatedocs.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
+echo "Installing necessary tools" &&
+sudo su -c "sudo npm -g install widdershins" &&
+sudo su -c "sudo npm -g install shins" &&
+echo "Setting up configurations" &&
+sudo cp logo.png /usr/lib/node_modules/shins/source/images/logo.png &&
+sudo cp screen.css /usr/lib/node_modules/shins/pub/css/screen_overrides.css &&
+echo "Building markdown" &&
 sudo widdershins swagger.yaml -o doc-shins-markdown.md --verbose -u widdershins-templates/openapi3/ &&
-sudo shins --inline --customcss doc-shins-markdown.md
+echo "Rendering HTML" &&
+sudo shins doc-shins-markdown.md --inline --customcss
